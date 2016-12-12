a122dbd313eb8e64aaa40d3d0137d3806538945a
igordanilchik <igor1968>
2016-12-12 11:55:51 +0300

    Migrate back to retro lambdas

--- app/src/main/java/com/igordanilchik/android/rxandroid_test/RxAndroidApplication.java
+++ app/src/main/java/com/igordanilchik/android/rxandroid_test/RxAndroidApplication.java
@@ -1,17 +1,31 @@
 package com.igordanilchik.android.rxandroid_test;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.os.StrictMode;
 
 import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 public class RxAndroidApplication extends Application {
+
+    public static RefWatcher getRefWatcher(Context context) {
+        RxAndroidApplication application = (RxAndroidApplication) context.getApplicationContext();
+        return application.refWatcher;
+    }
+
+    private RefWatcher refWatcher;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        LeakCanary.install(this);
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        refWatcher = LeakCanary.install(this);
 
         final boolean isDebuggable = 0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE);
 
